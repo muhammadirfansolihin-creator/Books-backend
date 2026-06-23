6fcf7e7cc584423cd6ae853282d4d9bd97a059e4
Irfan <muhammadirfansolihin@graduate.utm.my>
2026-06-23 19:09:31 +0800

    .

--- src/Controllers/AuthController.php
+++ src/Controllers/AuthController.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Controllers;
 use App\Auth\JwtService;
+use App\Repositories\AuditLog;
 use App\Repositories\UserRepository;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
